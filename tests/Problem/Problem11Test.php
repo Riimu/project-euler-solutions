528aab012b046ee9fbabbf6ac924c660a91075ab
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2026-04-29 16:40:53 +0300

    Write initial solution for problem 11

---
+++ tests/Problem/Problem11Test.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Riimu\EulerSolver\Test\Problem;
+
+use PHPUnit\Framework\TestCase;
+use Riimu\EulerSolver\Problem\Problem11;
+
+/**
+ * @author Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
+ * @copyright Copyright (c) 2026 Riikka Kalliomäki
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class Problem11Test extends TestCase
+{
+    public function testProblem2Solution(): void
+    {
+        $solver = new Problem11();
+        self::assertSame('70600674', $solver->solve());
+    }
+}
