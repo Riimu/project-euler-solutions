e0ef9057c77deeebf0f31025035d40df07c22ea4
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2026-05-01 12:59:04 +0300

    Add solution for problem 13

---
+++ tests/Problem/Problem13Test.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Riimu\EulerSolver\Test\Problem;
+
+use PHPUnit\Framework\TestCase;
+use Riimu\EulerSolver\Problem\Problem13;
+
+/**
+ * @author Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
+ * @copyright Copyright (c) 2026 Riikka Kalliomäki
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class Problem13Test extends TestCase
+{
+    public function testProblemSolution(): void
+    {
+        $solver = new Problem13();
+        self::assertSame('5537376230', $solver->solve());
+    }
+}
